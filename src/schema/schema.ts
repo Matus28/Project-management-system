43f8fdeaf7adaabfcd03351bfeda733ee47ced95
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-11 23:22:20 +0200

    feat: creates basic schema, only for clients for now

---
+++ src/schema/schema.ts
@@ -0,0 +1,37 @@
+import mockdata from "../mockdata/sampleData";
+import {
+  GraphQLID,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+
+const { projects, clients } = mockdata;
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({
+  query: RootQuery,
+});
