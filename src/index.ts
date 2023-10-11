75af47eec31b0001f3688e2a196c4fb59b40ff72
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-11 23:21:16 +0200

    feat: creates and configs web app, sets imports and listenning on the port

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./schema/schema";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, () => {
+  console.log(
+    `[api]: Server is running at http://localhost:${process.env.PORT}`
+  );
+});
