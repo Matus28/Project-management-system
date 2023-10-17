0ca3a6241bb20bb5428b5457148d09f9341ced2f
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-17 19:03:31 +0200

    feat: adds new Spinner component using bootstrap to show spinner while loading data

---
+++ client/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = (): JSX.Element => {
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="spinner-border" role="status"></div>
+    </div>
+  );
+};
